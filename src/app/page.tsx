d9807270bbe899ac1e7cf54ef23ecd05bb3aa3d5
jackblatch <jack@26385.xyz>
2023-05-03 14:09:46 +0100

    add example env and tweak ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,24 @@ export default async function Home() {
   console.log(session);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-top p-24 text-center">
+      <h1 className="text-2xl font-semibold mb-6">
+        Authjs with Nextjs App Router
+      </h1>
       {session?.user ? (
         <div>
-          <h2>Welcome {session.user.name}</h2>
+          <h2 className="text-xl mb-2">Welcome {session.user.name}</h2>
           <SignOut />
         </div>
       ) : (
-        <Link href="/api/auth/signin">
-          <button className="text-white bg-slate-800 py-2 px-6 rounded-md">
-            Login
-          </button>
-        </Link>
+        <div>
+          <h2 className="text-xl mb-2">Welcome</h2>
+          <Link href="/api/auth/signin">
+            <button className="text-white bg-slate-800 py-2 px-6 rounded-md">
+              Sign in
+            </button>
+          </Link>
+        </div>
       )}
     </main>
   );
--- src/components/signout.tsx
+++ src/components/signout.tsx
@@ -1,9 +1,13 @@
-"use client"
-import { signOut } from 'next-auth/react'
-import { PropsWithChildren } from 'react'
+"use client";
+import { signOut } from "next-auth/react";
 
 export const SignOut = () => {
-    return (
-        <button onClick={() => void signOut()}>Sign out</button>
-    )
-}
+  return (
+    <button
+      onClick={() => void signOut()}
+      className="text-white bg-slate-800 py-2 px-6 rounded-md"
+    >
+      Sign out
+    </button>
+  );
+};
